01bf601218f2b35f1f5b918a33a1b8091fea5673
= <eduardoenriqueescamilla@MacBook-Air-de-Eduardo.local>
2021-01-24 17:08:15 -0600

    Initial commit

---
+++ my-app/src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCRve5JZ2Wgcy5ZlfkL4sbFtAogZta_5GA",
+    authDomain: "employee-curd-app.firebaseapp.com",
+    projectId: "employee-curd-app",
+    storageBucket: "employee-curd-app.appspot.com",
+    messagingSenderId: "668149663531",
+    appId: "1:668149663531:web:2ea4e81eb58ff7b0bf676e"
+  };
+  // Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
